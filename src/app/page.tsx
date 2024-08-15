e9268103c5bedcf52839c847ab0885ebc72f85ed
Marcos David <mdgomes.net@hotmai.com>
2024-08-15 11:32:06 -0300

    feat: add Barbershopitem componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,13 @@ import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
 import { Badge } from "./_components/ui/badge";
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { db } from "./_lib/prisma";
+import BarbershopItem from "./_components/barbeshop-item";
 
-export default function Home() {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({});
+  console.log({ barbershops });
+
   return (
     <div>
       {/*header */}
@@ -32,14 +37,17 @@ export default function Home() {
           />
         </div>
         {/* Agendamento*/}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Esquerda*/}
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge>Confirmado</Badge>
               <h3>Corte de Cabelo</h3>
 
-              <div className="flex items-center gap-2">
+              <div className="flex items-center gap-2 rounded-2xl">
                 <Avatar className="h-6 w-6">
                   <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
                 </Avatar>
@@ -54,7 +62,14 @@ export default function Home() {
             </div>
           </CardContent>
         </Card>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        {barbershops.map((barbershop) => (
+          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+        ))}
       </div>
     </div>
   );
-}
+};
+export default Home;
