7962a9432de09cf8aa89d5783e2342e79e176cce
Marcos David <mdgomes.net@hotmai.com>
2024-08-14 23:01:38 -0300

    feat: add lookin item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
@@ -12,12 +15,14 @@ export default function Home() {
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá Marcos</h2>
         <p>Terça-Feira,14 de Agosto</p>
+        {/* Busca*/}
         <div className="flex items-center gap-2 mt-6">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
+        {/* Imagem*/}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             src="/banner01.png"
@@ -26,6 +31,29 @@ export default function Home() {
             alt={"Agende dos melhores com FSW"}
           />
         </div>
+        {/* Agendamento*/}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge>Confirmado</Badge>
+              <h3>Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/*Direita*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">14</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
